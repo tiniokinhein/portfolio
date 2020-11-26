ef5a5e39e3024184cde4acdc921a9375c8bcb7d3
Tin Ko Hein <tinkohein.oo@gmail.com>
2020-11-26 17:42:10 +0630

    Update experiences.js

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -42,7 +42,7 @@ const Experiences = () => (
                     <small>Myanmar</small><br />
                     <small><strong>Aug 2016 — Nov 2020</strong></small><br />
                 </div>
-//                 <div className="col-12 col-sm-6">
+                <div className="col-12 col-sm-6">
 //                     <p className="text-secondary mb-4" style={{
 //                             fontSize: '25px',
 //                             fontStyle: 'italic'
@@ -51,7 +51,7 @@ const Experiences = () => (
 //                     <small><a href="https://bbg.com.mm" target="_blank" className="text-decoration-none text-dark" rel="noopener noreferrer"><strong>Bagan Business Group Co. Ltd,</strong></a></small><br />
 //                     <small>Myanmar</small><br />
 //                     <small><strong>Aug 2016 — Nov 2020</strong></small><br />
-//                 </div>
+                </div>
             </div>
         </div>
     </Layout>
