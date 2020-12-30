a33ae5e441a0bcb0078b66e0bf852a6d38196232
Tin Ko Hein <tinkohein.oo@gmail.com>
2020-12-30 15:16:34 +0630

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -28,62 +28,62 @@ const Projects = () => (
         <SEO title="Working Experiences" />
         <div className="site-name">
             <div className="row">
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="https://baganstore.netlify.app" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={BS} alt="Bagan Store" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="https://zayweb.netlify.app" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={BZ} alt="Bagan Zay" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
     
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="http://frontiertechnologypartners.com" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={FTP} alt="Frontier Technology Partners" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="https://janamon.co/" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={Janamon} alt="Jana Mon - Ethnic Mon Cuisine" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="https://austchammyanmar.com/" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={AustCham} alt="AustCham Myanmar" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="http://jitmyanmar.com/" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={JIT} alt="JITIM Co., Ltd." className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="https://yatkwat.netlify.com/" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={Yatkwat} alt="YatKwat" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="https://www.ssdsw.gov.mm" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={SSDSW} alt="Department Of Social Welfare" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="https://www.dsw.gov.mm" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={DSW} alt="Department Of Social Welfare" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
                     </a>
                 </div>
-                <div className="col-6 col-md-4 mb-3">
+                <div className="col-6 col-md-3 mb-3">
                     <a href="https://bbg.com.mm" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                         <img src={BBG} alt="Bagan Business Group" className="mb-0 border" />
                         <p className="border border-top-0 m-0 px-3 py-4 bg-white text-dark text-decoration-none text-center site-name text-uppercase"><small>Visit</small></p>
